f487564ab1ec5c7e2faf2e8f208f54e429509e77
Mahadev Kumar <rajmahadev422@gmail.com>
2026-08-01 20:06:20 +0530

    Time: 3 ms (39.21%) | Memory: 9.5 MB (85.12%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -3,6 +3,7 @@ public:
     bool predict(int l, int r, int a, int b, vector<int> &v, bool turn) {
 
         if(l > r) return a >= b;
+        
         if(turn) return predict(l + 1, r, a + v[l], b, v, !turn) or predict(l, r - 1, a + v[r], b, v, !turn);
         return predict(l + 1, r, a, b + v[l], v, !turn) and predict(l, r - 1, a, b + v[r], v, !turn);
     }
