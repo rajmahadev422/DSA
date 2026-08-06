19ffee381aed44d31146f50ae49b21a7a8f419ec
Mahadev Kumar <rajmahadev422@gmail.com>
2026-08-06 10:16:53 +0530

    Time: 10 ms (76.72%) | Memory: 30.1 MB (78.44%) - LeetSync

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int n = s.size();
+
+        if (n & 1) return false;
+
+        int bal = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (locked[i] == '0' || s[i] == '(')
+                bal++;
+            else
+                bal--;
+
+            if (bal < 0)
+                return false;
+        }
+
+        bal = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (locked[i] == '0' || s[i] == ')')
+                bal++;
+            else
+                bal--;
+
+            if (bal < 0)
+                return false;
+        }
+
+        return true;
+    }
+};
