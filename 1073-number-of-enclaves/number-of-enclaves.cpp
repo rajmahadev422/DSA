0c1915d2bc6b67d658db9ad8d970451357275efc
Mahadev Kumar <rajmahadev422@gmail.com>
2026-08-08 08:52:27 +0530

    Time: 4 ms (89.69%) | Memory: 35.9 MB (39.67%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-
-    void dfs(vector<vector<int>>& grid, int i, int j, int &tot) {
-        if(i < 0 or j < 0 or i >= grid.size() or j >= grid[i].size() or grid[i][j] == 0) return;
+    void dfs(vector<vector<int>>& grid, int i, int j, int& tot) {
+        if (i < 0 or j < 0 or i >= grid.size() or j >= grid[i].size() or
+            grid[i][j] == 0)
+            return;
 
         grid[i][j] = 0;
         tot--;
@@ -15,28 +16,25 @@ public:
     int numEnclaves(vector<vector<int>>& grid) {
         int tot = 0;
 
-        for(int i = 0;i < grid.size();i++) {
-            for(int ele:grid[i]) if(ele == 1) tot++;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int ele : grid[i])
+                if (ele == 1)
+                    tot++;
         }
 
-        for(int i = 0;i < 1;i++) {
-            for(int j = 0;j < grid[i].size();j++) {
-                if(grid[i][j] == 1) {
-                    grid[i][j] = 0;
-                    tot--;
-                    dfs(grid, i + 1, j, tot);
-                    dfs(grid, i - 1, j, tot);
-                    dfs(grid, i, j + 1, tot);
-                    dfs(grid, i, j - 1, tot);
-                }
-            }
-        };
+        int n = grid.size();
+        int m = grid[0].size();
 
-        for(int i = grid.size() - 1;i < grid.size();i++) {
-            for(int j = 0;j < grid[i].size();j++) {
-                if(grid[i][j] == 1) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (i != 0 && i != n - 1 && j != 0 && j != m - 1)
+                    continue;
+
+                if (grid[i][j] == 1) {
                     grid[i][j] = 0;
                     tot--;
+
                     dfs(grid, i + 1, j, tot);
                     dfs(grid, i - 1, j, tot);
                     dfs(grid, i, j + 1, tot);
@@ -44,32 +42,6 @@ public:
                 }
             }
         }
-        for(int i = 0;i < grid.size();i++) {
-            for(int j = 0;j < 1;j++) {
-                if(grid[i][j] == 1) {
-                    grid[i][j] = 0;
-                    tot--;
-                    dfs(grid, i + 1, j, tot);
-                    dfs(grid, i - 1, j, tot);
-                    dfs(grid, i, j + 1, tot);
-                    dfs(grid, i, j - 1, tot);
-                }
-            }
-        };
-
-        for(int i = 0;i < grid.size();i++) {
-            for(int j = grid[i].size() - 1;j < grid[i].size();j++) {
-                if(grid[i][j] == 1) {
-                    grid[i][j] = 0;
-                    tot--;
-                    dfs(grid, i + 1, j, tot);
-                    dfs(grid, i - 1, j, tot);
-                    dfs(grid, i, j + 1, tot);
-                    dfs(grid, i, j - 1, tot);
-                }
-            }
-        }
-
 
         return tot;
     }
