1d8a2eeff7e7ddcacb7380d6c6d595dc262d426d
Mahadev Kumar <rajmahadev422@gmail.com>
2026-08-15 09:37:44 +0530

    Time: 3 ms (43.09%) | Memory: 171.3 MB (56.25%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
     unordered_map<int, int> dp;
-    void solve(int idx, int x, int n, vector<int> & nums, priority_queue<int, vector<int>, greater<int> > &pq) {
-        if(dp.contains(idx) and dp[idx] >= n) return;
-        if(x != 0) {
-            pq.push(n);
-            if(pq.size() == 2) pq.pop();
-            dp[idx] = n;
-        }
-        if(idx >= nums.size()) return;
-
-        solve(idx + 1, x ^ nums[idx], n + 1, nums, pq);
-        solve(idx + 1, x, n, nums, pq);
-    }
+    
     int longestSubsequence(vector<int>& nums) {
         int cnt = 0, x = 0;
         for(int i = 0;i < nums.size();i++) {
@@ -22,9 +11,6 @@ public:
         if(x != 0) return nums.size();
         if(cnt == nums.size()) return 0;
         
-        priority_queue<int, vector<int>, greater<int> > pq;
-        solve(0, 0, 0, nums, pq);
-        if(pq.size() == 2) pq.pop();
-        return pq.top();
+        return nums.size() - 1;
     }
 };
