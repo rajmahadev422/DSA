4100392118addd244fc5917fffb30eb74c3b8dcf
Mahadev Kumar <rajmahadev422@gmail.com>
2026-08-30 23:17:44 +0530

    Time: 1269 ms (44.44%) | Memory: 478.7 MB (33.33%) - LeetSync

--- 4373-maximum-valid-split-positions-i/maximum-valid-split-positions-i.cpp
+++ 4373-maximum-valid-split-positions-i/maximum-valid-split-positions-i.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    int maxValidSplits(vector<int>& nums) {
+    int solve(int idx, vector<int>& v) {
+        vector<int> temp = v;
 
-        int res = 0;
-        vector<int> temp = nums;
+        if (idx != -1)
+            temp.erase(temp.begin() + idx);
 
         int n = temp.size();
         vector<int> pf(n);
@@ -23,32 +24,13 @@ public:
             if (sf[i] == pf[i - 1])
                 ans++;
         }
-        res = max(res, ans);
-        for (int k = 0; k < nums.size(); k++) {
-            temp = nums;
+        return ans;
+    }
+    int maxValidSplits(vector<int>& nums) {
 
-            temp.erase(temp.begin() + k);
-
-            int n = temp.size();
-            vector<int> pf(n);
-            vector<int> sf(n);
-
-            pf[0] = temp[0];
-            sf[n - 1] = temp[n - 1];
-
-            for (int i = 1; i < n; i++) {
-                pf[i] = gcd(pf[i - 1], temp[i]);
-            }
-
-            for (int i = n - 2; i >= 0; i--)
-                sf[i] = gcd(sf[i + 1], temp[i]);
-            int ans = 0;
-            for (int i = 1; i < n; i++) {
-                if (sf[i] == pf[i - 1])
-                    ans++;
-            }
-            res = max(res, ans);
-        }
+        int res = 0;
+        
+        for (int k = -1; k < (int)nums.size(); k++) res = max(res, solve(k, nums));
         return res;
     }
 };
