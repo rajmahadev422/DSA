6026c09947516910b2a092591e8dda910a082e4e
Mahadev Kumar <rajmahadev422@gmail.com>
2026-09-09 11:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (19.20%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        long long ans = 0, c = 0;
+        long long c = 0;
         long long a1 = 1e3 - 1, a2 = 1e6 - 1, a3 = 1e9 - 1, a4 = 1e12 - 1, a5 = 1e15 - 1;
 
-        ans = max(c, (n - a5)*5) + max(c, (min(n, a5) - a4)*4) + max(c, (min(n, a4) - a3)*3) + max(c, (min(n, a3) - a2)*2) + max(c, (min(n, a2) - a1));
-        return ans;
+        return (max(c, (n - a5)*5) + max(c, (min(n, a5) - a4)*4) + max(c, (min(n, a4) - a3)*3) + max(c, (min(n, a3) - a2)*2) + max(c, (min(n, a2) - a1)));
     }
 };
